db8c661ac1763efa59724b1b0debe48964b84a79
Rokibul Islam <mymainac2022@gmail.com>
2022-12-18 03:10:22 +0600

    Create Project and FolderStructure

---
+++ Controllers/BookController.cs
@@ -0,0 +1,29 @@
+﻿using EasyBookShop.Models;
+using EasyBookShop.Repositary;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EasyBookShop.Controllers
+{
+    public class BookController : Controller
+    {
+        private readonly BookRepositary _bookRepositary = null;
+
+        public BookController()
+        {
+            _bookRepositary = new BookRepositary();
+        }
+            
+         public List<BookModel> GetAllBook()
+            {
+              return _bookRepositary.GetAllBook();
+            }
+        public BookModel GetById(int id)
+        {
+            return _bookRepositary.GetById(id);
+        }
+        public List<BookModel> SearchBook(string tittle, string authorName)
+        {
+            return _bookRepositary.SearchBook(tittle, authorName);
+        }
+    }
+}
---
+++ Repositary/BookRepositary.cs
@@ -0,0 +1,31 @@
+﻿using EasyBookShop.Models;
+
+namespace EasyBookShop.Repositary
+{
+    public class BookRepositary
+    {
+        public List<BookModel> GetAllBook()
+        {
+            return DataSource();
+        }
+        public BookModel GetById(int id)
+        {
+            return DataSource().Where(x => x.Id == id).FirstOrDefault();
+        }
+
+        public List<BookModel>SearchBook(string tittle,string authorName)
+        {
+            return DataSource().Where(x => x.Title.Contains(tittle) || x.Author.Contains(authorName)).ToList();
+        }
+        private List<BookModel> DataSource()
+        {
+            return new List<BookModel>()
+            {
+                new BookModel() { Id = 1, Title = "C#", Author = "Bulbul" },
+                new BookModel() { Id = 1, Title = "C", Author = "Babul" },
+                new BookModel() { Id = 1, Title = "C++", Author = "Bulbuli" }
+
+            };
+        }
+    }
+}
